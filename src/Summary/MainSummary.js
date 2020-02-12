b4c37f2e8077c273f66b355f4d78099f5f90c1cd
Michael Sanders <mikesanderspsu@gmail.com>
2020-02-11 20:24:30 -0500

    Added Summary Files

---
+++ src/Summary/MainSummary.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Summary from './Summary';
+
+
+class Cart extends React.Component {
+    render() {
+            const total = Object.keys(this.props.selected).reduce(
+                (acc, curr) => acc + this.props.selected[curr].cost,
+                0
+              );
+            return (
+            <section className="main__summary">
+                <h2>Your cart</h2>
+                <Summary
+                    selected={this.props.selected}
+                />
+                <div className="summary__total">
+                    <div className="summary__total__label">Total</div>
+                    <div className="summary__total__value">
+                        ${total}
+                    </div>
+                </div>
+            </section>
+        )
+    }
+}
+
+export default Cart
